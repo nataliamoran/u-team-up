832bbf12950e9573476c3ae51e464a884a8e190d
Tusooa Zhu <tusooa@vista.aero>
2020-02-22 15:08:24 -0500

    Implement swtiching months in calendar

--- u_team_up_app/src/react-components/Calendar/index.js
+++ u_team_up_app/src/react-components/Calendar/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 import './styles.css';
 
 const debug = console.log;
+
+const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 class Calendar extends React.Component {
     constructor(props) {
@@ -19,6 +23,16 @@ class Calendar extends React.Component {
             today: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 2),
             displayDate,
         };
+
+        this.changeMonth = this.changeMonth.bind(this);
+    }
+
+    changeMonth(diff) {
+        const { displayDate } = this.state;
+
+        displayDate.setMonth(displayDate.getMonth() + diff);
+
+        this.setState({ displayDate });
     }
 
     render() {
@@ -55,42 +69,59 @@ class Calendar extends React.Component {
         
         return (
             <div className='calendar'>
-                <table>
-                    <tbody>
-                        <th className='calendar__header'>
-                            <td>Sun</td><td>Mon</td><td>Tue</td><td>Wed</td>
-                            <td>Thu</td><td>Fri</td><td>Sat</td>
-                        </th>
-                        {
-                            Array(numWeeks(this.state.displayDate)).fill(0)
-                            .map((_, week) =>
-                                 <tr className='calendar__week' key={ week }>
-                                     {
-                                         [0, 1, 2, 3, 4, 5, 6].map(day => {
-                                             debug('w=', week);
-                                             const d = new Date(this.state.displayDate.getTime());
-                                             debug('d=', d);
-                                             const dayCode = firstDayNum(d) + week * 7 + day;
-                                             d.setDate(dayCode);
+                <div className='calendar__button_container'>
+                    <Button className='calendar__button_left'
+                            variant='outlined'
+                            color='primary'
+                            onClick={ () => this.changeMonth(-1) }> {'<'} </Button>
+                    <Button className='calendar__button_right'
+                            variant='outlined'
+                            color='primary'
+                            onClick={ () => this.changeMonth(+1) }> {'>'} </Button>
+                </div>
+                <div className='calendar__container'>
+                    <table>
+                        <tbody>
+                            <tr className='calendar__title'>
+                                <th colspan='7' className='calendar__title'>
+                                    { monthNames[this.state.displayDate.getMonth()] }&nbsp;
+                                    { this.state.displayDate.getFullYear() }</th>
+                            </tr>
+                            <tr className='calendar__header'>
+                                <th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th>
+                                <th>Thu</th><th>Fri</th><th>Sat</th>
+                            </tr>
+                            {
+                                Array(numWeeks(this.state.displayDate)).fill(0)
+                                .map((_, week) =>
+                                     <tr className='calendar__week' key={ week }>
+                                         {
+                                             [0, 1, 2, 3, 4, 5, 6].map(day => {
+                                                 debug('w=', week);
+                                                 const d = new Date(this.state.displayDate.getTime());
+                                                 debug('d=', d);
+                                                 const dayCode = firstDayNum(d) + week * 7 + day;
+                                                 d.setDate(dayCode);
 
-                                             const isDisplayMonth = d.getMonth() === this.state.displayDate.getMonth();
-                                             const isToday = d.getMonth() === this.state.today.getMonth()
-                                                   && d.getFullYear() === this.state.today.getFullYear()
-                                                   && d.getDate() === this.state.today.getDate();
-                                             const classes = 'calendar__day'
-                                                   + (isDisplayMonth ? '' : ' calendar__day_inactive')
-                                                   + (isToday ? ' calendar__day_today' : '');
+                                                 const isDisplayMonth = d.getMonth() === this.state.displayDate.getMonth();
+                                                 const isToday = d.getMonth() === this.state.today.getMonth()
+                                                       && d.getFullYear() === this.state.today.getFullYear()
+                                                       && d.getDate() === this.state.today.getDate();
+                                                 const classes = 'calendar__day'
+                                                       + (isDisplayMonth ? '' : ' calendar__day_inactive')
+                                                       + (isToday ? ' calendar__day_today' : '');
 
-                                             return (<td key={ dayCode }
-                                                         className={ classes }>
-                                                         { d.getDate() }
-                                                     </td>);
-                                         })
-                                     }
-                                 </tr>)
-                        }
-                    </tbody>
-                </table>
+                                                 return (<td key={ dayCode }
+                                                       className={ classes }>
+                                    { d.getDate() }
+                                </td>);
+                                             })
+                                         }
+                                     </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
