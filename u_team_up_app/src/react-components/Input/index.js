7abecf7d31dc64b45aea8391638d4eaf42e8743c
Tusooa Zhu <tusooa@vista.aero>
2020-02-23 16:04:12 -0500

    Make Input accept className and value

--- u_team_up_app/src/react-components/Input/index.js
+++ u_team_up_app/src/react-components/Input/index.js
@@ -5,7 +5,7 @@ import Grid from "@material-ui/core/Grid";
 
 class Input extends React.Component {
     render() {
-        const { label, value, onChange, name } = this.props;
+        const { label, value, onChange, name, className } = this.props;
 
         return (
             <Grid item xl={3} lg={3} md={4} s={12} xs={12}>
@@ -13,8 +13,8 @@ class Input extends React.Component {
                     name={name}
                     label={label}
                     id="margin-normal"
-                    defaultValue={value || ""}
-                    className="input"
+                    value={value || ""}
+                    className={"input" + (className ? ' ' + className : '')}
                     margin="normal"
                     onChange={onChange}
                 />
