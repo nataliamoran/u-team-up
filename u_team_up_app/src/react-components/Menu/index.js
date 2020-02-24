8810b26a6316b7f4fb4d622bdcb00a677633e3a7
Tusooa Zhu <tusooa@vista.aero>
2020-02-24 17:22:53 -0500

    Revert to the old menu
    The old menu involves a css file that is
    breaking everything on the page. We should
    not use it.

--- u_team_up_app/src/react-components/Menu/index.js
+++ u_team_up_app/src/react-components/Menu/index.js
@@ -1,32 +1,57 @@
-import React, { Component } from "react";
-import { Menu, Message } from "semantic-ui-react";
+import React from "react";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
+import {Link} from "react-router-dom";
 
-export default class AppMenu extends Component {
-  state = {};
-  handleClick = () => this.setState({ message: "onClick handled" });
+export default function AppMenu(props) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-  render() {
-    const { message } = this.state;
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
-      <div>
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css"></link>
-        <Menu>
-          <Menu.Item href="/" target="_blank">
-            Search Teams
-          </Menu.Item>
-          <Menu.Item href="/search-student" target="_blank">
-            Search Students
-          </Menu.Item>
-          <Menu.Item href="/appointments" target="_blank">
-            Your Appointments
-          </Menu.Item>
-        </Menu>
-
-        {message && <Message content={message} />}
-      </div>
-
+            <div className="main_menu">
+                <Button className="main_menu_button" aria-controls="simple-menu" aria-haspopup="true"
+                        onClick={handleClick}>
+                    menu
+                </Button>
+                <Menu
+                    className="main_menu_content"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    {
+                        props.loginStatus === 'guest' ?
+                            [<Link to='/login'>
+                                <MenuItem>Log in</MenuItem>
+                             </Link>,
+                             <Link to='/signup'>
+                                 <MenuItem>Sign up</MenuItem>
+                             </Link>
+                            ] :
+                            [<Link to={"/"}>
+                                 <MenuItem>search teams</MenuItem>
+                             </Link>,
+                             <Link to={"/search-student"}>
+                                 <MenuItem>search students</MenuItem>
+                             </Link>,
+                             <Link to={"/appointments"}>
+                                 <MenuItem>your appointments</MenuItem>
+                             </Link>,
+                             <Link to='/logout'>
+                                 <MenuItem>Log out</MenuItem>
+                             </Link>,
+                            ]
+                    }
+                </Menu>
+            </div>
     );
-  }
 }
-
