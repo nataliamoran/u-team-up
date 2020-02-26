2a66446f1d9219fce2af8eeacc658647e013388c
X W <35614222+XinmengWang@users.noreply.github.com>
2020-02-26 07:06:25 -0500

    Improved SignUp

--- u_team_up_app/src/react-components/SignUp/index.js
+++ u_team_up_app/src/react-components/SignUp/index.js
@@ -2,47 +2,106 @@ import React from "react";
 import "./styles.css";
 
 class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: "",
+            university: "",
+            email: "",
+            password: "",
+            confirmedPassword: ""
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        alert('Registered!')
+        e.preventDefault();
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
-      <div>
-        <meta charSet="utf-8" />
-        <title>SignUp</title>
+            <div>
+            <title>Signup</title>
 
-        <div className="content">
-          <h1>UTeamUp SignUp</h1>
-          <form className="login" action="./../SearchTeam" method="post">
-            {/* UTORid */}
-            <label htmlFor="utorid">UTORID</label>
-            <br />
-            <input className="login_input" type="text" name="utorid" defaultValue />
-            <br /><br />
+                <div className="signup__content">
+                  <h1>Create an account</h1>
 
-            <label htmlFor="uni">UNIVERSITY</label>
-            <br />
-            <input className="login_input" type="text" name="uni" defaultValue />
-            <br /><br />
+                  <form className="signup" action="./../SearchTeam" method="post" onSubmit={this.handleSubmit}>
+                    <label htmlFor="username">USERNAME</label>
+                    <br />
+                    <input
+                        className="signup__input"
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <br /><br />
 
-            <label htmlFor="email">UNIVERSITY EMAIL</label>
-            <br />
-            <input className="login_input" type="text" name="email" defaultValue />
-            <br /><br />
+                    <label htmlFor="uni">UNIVERSITY</label>
+                    <br />
+                    <input
+                        className="signup__input"
+                        type="text"
+                        name="university"
+                        value={this.state.university}
+                        onChange={this.handleChange}
+                    />
+                    <br /><br />
 
-            {/* password */}
-            <label htmlFor="password">PASSWORD</label>
-            <br />
-            <input className="login_input" type="text" name="password" defaultValue />
-            <br /><br />
+                    <label htmlFor="email">UNIVERSITY EMAIL</label>
+                    <br />
+                    <input
+                        className="signup__input"
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    <br /><br />
 
-            <label htmlFor="confirm_password">CONFIRM PASSWORD</label>
-            <br />
-            <input className="login_input" type="text" name="confirm_password" defaultValue />
-            <br /><br />
+                    <label htmlFor="password">PASSWORD</label>
+                    <br />
+                    <input
+                        className="signup__input"
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <br /><br />
 
-            {/* login button */}
-            <input className="login_button" type="submit" name="signup" defaultValue="Signup" />
-          </form>
-        </div>
-      </div>
+                    <label htmlFor="confirm_password">CONFIRM PASSWORD</label>
+                    <br />
+                    <input
+                        className="signup__input"
+                        type="password"
+                        name="confirmedPassword"
+                        value={this.state.confirmedPassword}
+                        onChange={this.handleChange}
+                    />
+                    <br /><br />
+                    <br /><br />
+
+                    <button
+                        className="signup__button"
+                        type="submit"
+                        >
+                        SignUp
+                    </button>
+                  </form>
+                </div>
+            </div>
     );
     }
 }
