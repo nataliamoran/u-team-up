4f2fa5707d370754723c8e9bae5632bff9ea13c8
nataliamoran <natalja.moran@gmail.com>
2020-02-29 18:29:41 -0500

    hardcoded uid '1' for testing

--- u_team_up_app/src/App.js
+++ u_team_up_app/src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
             identity: {
                 type: 'guest',
                 username: '',
-                uid: '',
+                uid: '1',
             },
         };
 
