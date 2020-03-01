029a37d886b752cd34a0abd6cf67f12a50f82646
X W <35614222+XinmengWang@users.noreply.github.com>
2020-02-29 19:58:38 -0500

    Add StudentProfile edit mode
    (incomplete)

--- u_team_up_app/src/react-components/StudentProfile/index.js
+++ u_team_up_app/src/react-components/StudentProfile/index.js
@@ -3,42 +3,222 @@ import "./styles.css";
 import alice from "./static/alice.png"
 
 class StudentProfile extends React.Component {
-    render() {
+    state = {
+        username: "",
+        imageUrl: alice,
+        name: "Alice Alison",
+        university: "",
+        yearOfStudy: "",
+        majorOfStudy: "",
+        coursesTakenTags: ['CSC207', 'CSC209'],
+        currentCoursesTags: ['CSC309', 'CSC363', 'CSC401'],
+        currentTeams: ['CSC309 Team 1'],
+        reviews: [],
+        description: "If you love burger like I do, add me to your team.",
+        location: "On campus",
+        gpa: "3.5/4.0",
+        pastProject: "Todo list",
+        experience: ["Teaching Assistant @ UofT", "Software Developer Intern @ IBM"],
+        resume: "",
+        students: [ // TODO: FETCH
+
+        ],
+        isInEditMode: false,
+    }
+
+    renderTags() {
+        if (this.state.coursesTakenTags.length === 0) {
+            return null
+        } else {
+            return <ul>
+                    {this.state.coursesTakenTags.map(coursesTaken =>
+                    <li key={coursesTaken}>{ coursesTaken }</li>)}
+                    </ul>
+        }
+    }
+
+    updateInfo = () => {
+        this.setState({
+            isInEditMode: false,
+            name: this.refs.inputName.value,
+            description: this.refs.inputDescription.value,
+            location: this.refs.inputLocation.value,
+            gpa: this.refs.inputGpa.value,
+            pastProject: this.refs.inputPastProject.value,
+        })
+    }
+
+    changeEditMode = () => {
+        this.setState({
+            isInEditMode: !this.state.isInEditMode
+        })
+    }
+
+    renderEditView = () => {
         return (
             <div>
-                <meta charSet="utf-8" />
-                <title>Profile</title>
+                <div>
+                    <div className="left-box">
+                    <img className="profile-pic" src={this.state.imageUrl} alt="alice photo" />
+
+                    <div className="info inner">
+                        <h2>
+                            <input
+                                className="student_profile__input"
+                                type="text"
+                                defaultValue={this.state.name}
+                                ref="inputName"
+                                >
+                            </input>
+
+                        </h2>
+                        <p>Student at the Unviersity of Toronto</p>
+                        <p>3rd year, Computer Science Specialist</p>
+                    </div>
+
+                    <div className="course">
+                        <div className="taken inner">
+                          <h4>Courses Taken:</h4>
+                          {this.renderTags()}
+                        </div>
+
+                        <div className="taking inner">
+                          <h4>Currently Taking:</h4>
+                          <ul>
+                              {this.state.currentCoursesTags.map(currentCourses =>
+                                  <li key={currentCourses}>{ currentCourses }</li>)}
+                          </ul>
+                        </div>
+                    </div>
+
+                  <div className="current-teams inner">
+                    <h4>Current Teams:</h4>
+                    <ul>
+                        {this.state.currentTeams.map(currentTeams =>
+                            <li key={currentTeams}>{ currentTeams }</li>)}
+                    </ul>
+                  </div>
+
+                  <div className="reviews inner">
+                    <h4>Reviews:</h4>
+                    <p><a href="bob.html">Bob Bobson</a>: I love working with Alice!💙</p>
+                  </div>
+                </div>
+                <div className="right-box">
+                  <div className="notifications">
+                    <p><a href="student_application_invitation.html">Notifications</a></p>
+                  </div>
+
+                  <div className="description inner">
+                    <h4>Description:</h4>
+                    <p>
+                        <textarea
+                            className="student_profile__input"
+                            type="text"
+                            defaultValue={this.state.description}
+                            ref="inputDescription"
+                            >
+                        </textarea>
+
+                    </p>
+                  </div>
+
+                  <div className="more-info inner">
+                    <h4>Location:</h4>
+                        <input
+                            className="student_profile__input"
+                            type="text"
+                            defaultValue={this.state.location}
+                            ref="inputLocation"
+                            >
+                        </input>
+
+                    <h4>GPA:</h4>
+                        <input
+                            className="student_profile__input"
+                            type="text"
+                            defaultValue={this.state.gpa}
+                            ref="inputGpa"
+                            >
+                        </input>
+
+                    <h4>Past Project:</h4>
+                        <textarea
+                            className="student_profile__input"
+                            type="text"
+                            defaultValue={this.state.pastProject}
+                            ref="inputPastProject"
+                            >
+                        </textarea>
+
+                  </div>
+
+                  <div className="experience inner">
+                    <h4>Experience:</h4>
+                    <ul>
+                        {this.state.experience.map(experience =>
+                            <li key={experience}>{ experience }</li>)}
+
+                    </ul>
+                    <h4>Resume: Link</h4>
+                  </div>
+
+
+                </div>
+
+
+
+                <button
+                    className="student_profile__button"
+                    onClick={this.updateInfo}>Save
+                </button>
+
+                {/* Click the cancle button to go back to default mode*/}
+                <button
+                    className="student_profile__button"
+                    onClick={this.changeEditMode}>Cancle</button>
+                </div>
+            </div>
+
+        )
+    }
+
+
+    render() {
+        return (
+            this.state.isInEditMode ?
+            this.renderEditView()
+             :
+            <div>
                 <div className="left-box">
-                <img className="profile-pic" src={alice} alt="alice photo" />
+                <img className="profile-pic" src={this.state.imageUrl} alt="alice photo" />
 
                 <div className="info inner">
-                <h2>Alice Alison</h2>
-                <p>Student at the University of Toronto</p>
-                <p>3rd year, Computer Science Specialist</p>
-              </div>
+                    <h2>{this.state.name}</h2>
+                    <p>Student at the Unviersity of Toronto</p>
+                    <p>3rd year, Computer Science Specialist</p>
+                </div>
 
-              <div className="course">
-                <div className="taken inner">
-                  <h4>Courses Taken:</h4>
-                  <ul>
-                    <li>CSC207</li>
-                    <li>CSC209</li>
-                  </ul>
+                <div className="course">
+                    <div className="taken inner">
+                      <h4>Courses Taken:</h4>
+                      {this.renderTags()}
+                    </div>
+
+                    <div className="taking inner">
+                      <h4>Currently Taking:</h4>
+                      <ul>
+                          {this.state.currentCoursesTags.map(currentCourses =>
+                              <li key={currentCourses}>{ currentCourses }</li>)}
+                      </ul>
+                    </div>
                 </div>
-                <div className="taking inner">
-                  <h4>Currently Taking:</h4>
-                  <ul>
-                    <li>CSC309</li>
-                    <li>CSC363</li>
-                    <li>CSC401</li>
-                  </ul>
-                </div>
-              </div>
 
               <div className="current-teams inner">
                 <h4>Current Teams:</h4>
                 <ul>
-                  <li>CSC309 Team 1</li>
+                    {this.state.currentTeams.map(currentTeams =>
+                        <li key={currentTeams}>{ currentTeams }</li>)}
                 </ul>
               </div>
 
@@ -47,7 +227,6 @@ class StudentProfile extends React.Component {
                 <p><a href="bob.html">Bob Bobson</a>: I love working with Alice!💙</p>
               </div>
             </div>
-
             <div className="right-box">
               <div className="notifications">
                 <p><a href="student_application_invitation.html">Notifications</a></p>
@@ -55,25 +234,33 @@ class StudentProfile extends React.Component {
 
               <div className="description inner">
                 <h4>Description:</h4>
-                <p>If you love burger like I do, add me to your team.</p>
+                <p>{this.state.description}</p>
               </div>
 
               <div className="more-info inner">
-                <h4>Location:</h4> On campus
-                <h4>GPA:</h4> 3.5/4.0
-                <h4>Past Project:</h4> Todo list
+                <h4>Location:</h4> {this.state.location}
+                <h4>GPA:</h4> {this.state.gpa}
+                <h4>Past Project:</h4> {this.state.pastProject}
               </div>
 
               <div className="experience inner">
                 <h4>Experience:</h4>
                 <ul>
-                  <li>Teaching Assistant</li>
+                    {this.state.experience.map(experience =>
+                        <li key={experience}>{ experience }</li>)}
+
                 </ul>
-                <h4>Resume: <a href="#">Link</a></h4>
+                <h4>Resume: Link</h4>
               </div>
 
-              <div className="edit inner">
-                <p>Click <button type="button" name="edit">here</button> to edit your profile</p>
+              <div className="edit inner" onClick={this.changeEditMode}>
+                <p>Click
+                    <button
+                        className="student_profile__button"
+                        type="button"
+                        name="edit"> here
+                    </button>
+                        to edit your profile</p>
               </div>
             </div>
           </div>
