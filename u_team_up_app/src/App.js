cc8ab832b267de4631a60b4dcd2c7f973070f535
nataliamoran <natalja.moran@gmail.com>
2020-03-01 14:55:01 -0500

    removed hardcoded uid '1' from App.js

--- u_team_up_app/src/App.js
+++ u_team_up_app/src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
             identity: {
                 type: 'guest',
                 username: '',
-                uid: '1',
+                uid: '',
             },
         };
 
