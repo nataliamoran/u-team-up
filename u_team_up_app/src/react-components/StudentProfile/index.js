73a0a57356a01143d4d43fc780e8371c83807106
X W <35614222+XinmengWang@users.noreply.github.com>
2020-03-02 02:49:07 -0500

    Updated StudentProfile

--- u_team_up_app/src/react-components/StudentProfile/index.js
+++ u_team_up_app/src/react-components/StudentProfile/index.js
@@ -10,15 +10,15 @@ class StudentProfile extends React.Component {
         university: "University of Toronto, St. George Campus",
         yearOfStudy: 3,
         majorOfStudy: "Computer Science",
-        coursesTakenTags: ['CSC207', 'CSC209'],
-        currentCoursesTags: ['CSC309', 'CSC363', 'CSC401'],
-        currentTeams: ['CSC309 Team 1'],
-        reviews: [],
+        coursesTaken: "CSC207, CSC209",
+        currentCourses: "CSC309, CSC363, CSC401",
+        currentTeams: "CSC309 Team 1",
+        reviews: "",
         description: "If you love burger like I do, add me to your team.",
         location: "On campus",
         gpa: "3.5/4.0",
         pastProject: "Todo list",
-        experience: ["Teaching Assistant @ UofT", "Software Developer Intern @ IBM"],
+        experience: "Teaching Assistant @ UofT, Software Developer Intern @ IBM",
         resume: "",
         students: [ // TODO: FETCH
 
@@ -34,10 +34,14 @@ class StudentProfile extends React.Component {
             university: this.refs.inputUniversity.value,
             yearOfStudy: this.refs.inputyearOfStudy.value,
             majorOfStudy: this.refs.inputmajorOfStudy.value,
+            coursesTaken: this.refs.inputcoursesTaken.value,
+            currentCourses: this.refs.inputCurrentCourses.value,
+            currentTeams: this.refs.inputCurrentTeams.value,
             description: this.refs.inputDescription.value,
             location: this.refs.inputLocation.value,
             gpa: this.refs.inputGpa.value,
             pastProject: this.refs.inputPastProject.value,
+            experience: this.refs.inputExperience.value
         })
     }
 
@@ -103,27 +107,36 @@ class StudentProfile extends React.Component {
                     <div className="course">
                         <div className="taken inner">
                           <h4>Courses Taken:</h4>
-                              <ul>
-                                {this.state.coursesTakenTags.map(coursesTaken =>
-                                <li key={coursesTaken}>{ coursesTaken }</li>)}
-                              </ul>
+                              <textarea
+                                  className="student_profile__input"
+                                  type="text"
+                                  defaultValue={this.state.coursesTaken}
+                                  ref="inputcoursesTaken"
+                                  >
+                              </textarea>
                         </div>
 
                     <div className="taking inner">
                          <h4>Currently Taking:</h4>
-                         <ul>
-                             {this.state.currentCoursesTags.map(currentCourses =>
-                                <li key={currentCourses}>{ currentCourses }</li>)}
-                         </ul>
+                             <textarea
+                                 className="student_profile__input"
+                                 type="text"
+                                 defaultValue={this.state.currentCourses}
+                                 ref="inputCurrentCourses"
+                                 >
+                             </textarea>
                     </div>
                     </div>
 
                   <div className="current-teams inner">
                     <h4>Current Teams:</h4>
-                    <ul>
-                        {this.state.currentTeams.map(currentTeams =>
-                            <li key={currentTeams}>{ currentTeams }</li>)}
-                    </ul>
+                        <textarea
+                            className="student_profile__input"
+                            type="text"
+                            defaultValue={this.state.currentTeams}
+                            ref="inputCurrentTeams"
+                            >
+                        </textarea>
                   </div>
 
                  <div className="reviews inner">
@@ -183,11 +196,13 @@ class StudentProfile extends React.Component {
 
                   <div className="experience inner">
                     <h4>Experience:</h4>
-                    <ul>
-                        {this.state.experience.map(experience =>
-                            <li key={experience}>{ experience }</li>)}
-
-                    </ul>
+                        <textarea
+                            className="student_profile__input"
+                            type="text"
+                            defaultValue={this.state.experience}
+                            ref="inputExperience"
+                            >
+                        </textarea>
                     <h4>Resume: Link</h4>
                   </div>
 
@@ -202,19 +217,15 @@ class StudentProfile extends React.Component {
                 {/* Click the cancle button to go back to default mode*/}
                 <button
                     className="student_profile__button"
-                    onClick={this.changeEditMode}>Cancle</button>
+                    onClick={this.changeEditMode}>Cancel</button>
                 </div>
             </div>
 
         )
     }
 
-
-    render() {
+    renderSelfView = () => {
         return (
-            this.state.isInEditMode ?
-            this.renderEditView()
-             :
             <div>
                 <div className="left-box">
                 <img className="profile-pic" src={this.state.imageUrl} alt="alice photo" />
@@ -230,26 +241,17 @@ class StudentProfile extends React.Component {
                 <div className="course">
                     <div className="taken inner">
                       <h4>Courses Taken:</h4>
-                          <ul>
-                            {this.state.coursesTakenTags.map(coursesTaken =>
-                            <li key={coursesTaken}>{ coursesTaken }</li>)}
-                          </ul>
+                          {this.state.coursesTaken}
                     </div>
                     <div className="taking inner">
                       <h4>Currently Taking:</h4>
-                      <ul>
-                          {this.state.currentCoursesTags.map(currentCourses =>
-                              <li key={currentCourses}>{ currentCourses }</li>)}
-                      </ul>
+                      {this.state.currentCourses}
                     </div>
                 </div>
 
               <div className="current-teams inner">
                 <h4>Current Teams:</h4>
-                <ul>
-                    {this.state.currentTeams.map(currentTeams =>
-                        <li key={currentTeams}>{ currentTeams }</li>)}
-                </ul>
+                {this.state.currentTeams}
               </div>
 
               <div className="reviews inner">
@@ -275,11 +277,8 @@ class StudentProfile extends React.Component {
 
               <div className="experience inner">
                 <h4>Experience:</h4>
-                <ul>
-                    {this.state.experience.map(experience =>
-                        <li key={experience}>{ experience }</li>)}
+                {this.state.experience}
 
-                </ul>
                 <h4>Resume: Link</h4>
               </div>
 
@@ -294,6 +293,74 @@ class StudentProfile extends React.Component {
               </div>
             </div>
           </div>
+        )
+    }
+
+    renderOthersView = () => {
+        return(
+            <div>
+                <div className="left-box">
+                <img className="profile-pic" src={this.state.imageUrl} alt="alice photo" />
+
+                <div className="info inner">
+                    <h2>{this.state.name}</h2>
+                    <h4 className="inline_h4">University:</h4> <p>{this.state.university}</p>
+                    <h4>Year of Study:</h4> <p>{this.state.yearOfStudy}</p>
+                    <h4>Major of Study:</h4> <p>{this.state.majorOfStudy}</p>
+
+                </div>
+
+                <div className="course">
+                    <div className="taken inner">
+                      <h4>Courses Taken:</h4>
+                          {this.state.coursesTaken}
+                    </div>
+                    <div className="taking inner">
+                      <h4>Currently Taking:</h4>
+                      {this.state.currentCourses}
+                    </div>
+                </div>
+
+              <div className="current-teams inner">
+                <h4>Current Teams:</h4>
+                {this.state.currentTeams}
+              </div>
+
+              <div className="reviews inner">
+                <h4>Reviews:</h4>
+                <p><a href="bob.html">Bob Bobson</a>: I love working with Alice!💙</p>
+              </div>
+            </div>
+            <div className="right-box">
+
+
+              <div className="description inner">
+                <h4>Description:</h4>
+                <p>{this.state.description}</p>
+              </div>
+
+              <div className="more-info inner">
+                <h4>Location:</h4> {this.state.location}
+                <h4>GPA:</h4> {this.state.gpa}
+                <h4>Past Project:</h4> {this.state.pastProject}
+              </div>
+
+              <div className="experience inner">
+                <h4>Experience:</h4>
+                {this.state.experience}
+
+                <h4>Resume: Link</h4>
+              </div>
+            </div>
+          </div>
+        )
+    }
+
+    render() {
+        return (
+            this.state.isInEditMode ?
+            this.renderEditView()
+             : this.renderSelfView()
         )
     }
 }
