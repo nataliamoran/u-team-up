70374f837e7d33b9d81fe93153679a8c592a5330
nataliamoran <natalja.moran@gmail.com>
2020-03-22 00:21:31 -0400

    removed proxi

--- u_team_up_app/src/config.js
+++ u_team_up_app/src/config.js
@@ -1,5 +1,5 @@
-const BASE_URL = '/api';
-// const BASE_URL = 'http://localhost:5000/api';
+// const BASE_URL = '/api';
+const BASE_URL = 'http://localhost:5000/api';
 export const TEAMS_BACKEND = BASE_URL + '/teams';
 export const USERS_BACKEND = BASE_URL + '/users';
 export const PROFILES_BACKEND = BASE_URL + '/profiles';
