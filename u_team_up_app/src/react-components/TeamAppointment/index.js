5583b63b130b0e1c731cb2d613b1bb7048b051e6
nataliamoran <natalja.moran@gmail.com>
2020-03-22 16:42:00 -0400

    Team Events are saved to DB / loaded from DB

--- u_team_up_app/src/react-components/TeamAppointment/index.js
+++ u_team_up_app/src/react-components/TeamAppointment/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Calendar from '../Calendar';
 import Header from '../Header';
 import { Link } from 'react-router-dom';
+import {updateTeamDataInDB} from "../../actions/teamScripts";
+import {TEAMS_BACKEND} from "../../config";
+import {uid} from "react-uid";
+import TextField from "@material-ui/core/TextField/TextField";
 
 const debug = console.log;
 
@@ -13,26 +17,60 @@ class TeamAppointment extends React.Component {
         super(props);
 
         this.state = {
+            dataLoaded: false,
             teamId: props.teamId || '', // teamId: String
             appointmentTime: {
                 start: null,
                 end: null
             }, // {start: Date, end: Date}
             otherSchedule: [ // TODO: FETCH
-                {
-                    name: 'Some Meeting',
-                    start: new Date(2020, 1, 20, 14, 30),
-                    end: new Date(2020, 1, 20, 15, 20)
-                },
-                {
-                    name: 'Something else',
-                    start: new Date(2020, 1, 22, 13, 0),
-                    end: new Date(2020, 1, 22, 14, 0)
-                }
+                // {
+                //     name: 'Some Meeting',
+                //     start: new Date(2020, 1, 20, 14, 30),
+                //     end: new Date(2020, 1, 20, 15, 20)
+                // },
+                // {
+                //     name: 'Something else',
+                //     start: new Date(2020, 1, 22, 13, 0),
+                //     end: new Date(2020, 1, 22, 14, 0)
+                // }
             ], // [{start: Date, end: Date, name: string}, ...]
         };
 
         this.addEvent = this.addEvent.bind(this);
+    }
+
+    getEventsFromJson = (eventsFromDB) => {
+        const events = [];
+        eventsFromDB.map(event => (
+            <div key={uid(
+                event
+            )}>
+                {events.push({
+                    name: event.name,
+                    start: new Date(event.start),
+                    end: new Date(event.end)
+                })}
+            </div>
+        ));
+        return events;
+    };
+
+    componentDidMount() {
+        const url = TEAMS_BACKEND + "/" + this.props.teamId;
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                const events = this.getEventsFromJson(json.events);
+                this.setState({
+                    otherSchedule: events,
+                    dataLoaded: true
+                });
+                console.log(this.state);
+            }).catch((error) => {
+            console.error(error)
+        });
     }
 
     // @param: ev: {start: Date, end: Date, name: string}
@@ -46,12 +84,18 @@ class TeamAppointment extends React.Component {
         this.setState({ otherSchedule });
 
         // TODO: UPLOAD
+        let data = {
+            events: otherSchedule
+        };
+        updateTeamDataInDB(data, this.state.teamId);
+
     }
     
     render() {
         const authorized = this.props.globalState.identity.type === 'user';
         
         return (
+            this.state.dataLoaded ?
             <div className="team_appointment">
                 <Header type='main' 
                         title='Team Appointment' />
@@ -71,6 +115,8 @@ class TeamAppointment extends React.Component {
                 ] : 'You are not allowed to visit this page. Please log in or sign up.'
                 }
             </div>
+                :
+                null
         );
     }
 };
