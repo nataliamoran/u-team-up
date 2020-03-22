ce69aed21741098a069c1d114b6918ee74843052
nataliamoran <natalja.moran@gmail.com>
2020-03-21 23:41:04 -0400

    updated api path in config

--- u_team_up_app/src/config.js
+++ u_team_up_app/src/config.js
@@ -1,2 +1,5 @@
-export const TEAMS_BACKEND = 'http://localhost:5000/teams';
-export const USERS_BACKEND = 'http://localhost:5000/users';
+const BASE_URL = '/api';
+// const BASE_URL = 'http://localhost:5000/api';
+export const TEAMS_BACKEND = BASE_URL + '/teams';
+export const USERS_BACKEND = BASE_URL + '/users';
+export const PROFILES_BACKEND = BASE_URL + '/profiles';
