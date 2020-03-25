76001c2709c0b99123600c760b2b50c5174e6a8b
nataliamoran <natalja.moran@gmail.com>
2020-03-24 20:06:16 -0400

    improved filterUnits method (for front-end search)

--- u_team_up_app/src/actions/filterUnits.js
+++ u_team_up_app/src/actions/filterUnits.js
@@ -1,11 +1,13 @@
 // Method to filter Student List or Team List based on the Criteria
+const stringSimilarity = require('string-similarity');
 
 export const filterUnits = (criteria, studentOrTeamList) => {
     return Object.keys(criteria).filter(c => criteria[c]).reduce(
         (accumulator, c) => accumulator.filter(unit =>
-            (unit[c] === criteria[c]) ||
-            (Array.isArray(unit[c]) &&
-            !(unit[c].filter(subUnit => subUnit === criteria[c]).length === 0))
+            (stringSimilarity.compareTwoStrings(unit[c].toLowerCase(), criteria[c].toLowerCase()) > 0.5)
+            || (Array.isArray(unit[c])
+            && !(unit[c].filter(
+                subUnit => (stringSimilarity.compareTwoStrings(subUnit.toLowerCase(), criteria[c].toLowerCase()) > 0.5)).length === 0))
         ),
         studentOrTeamList)
 };
