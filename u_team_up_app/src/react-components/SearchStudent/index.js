a16a0ba64b5acf4f798d26dfe61ae2cade87cd72
Tusooa Zhu <tusooa@vista.aero>
2020-04-01 15:17:38 -0400

    Remove duplicate code for admin view in search student

--- u_team_up_app/src/react-components/SearchStudent/index.js
+++ u_team_up_app/src/react-components/SearchStudent/index.js
@@ -5,7 +5,6 @@ import {filterUnits} from "../../actions/filterUnits";
 import SearchStudentForm from "./../SearchStudentForm";
 
 import TeamMemberPreviewList from "../TeamMemberPreviewList";
-import {uid} from "react-uid";
 import Grid from "@material-ui/core/Grid/Grid";
 import Input from "../Input";
 import Button from "@material-ui/core/Button/Button";
@@ -68,89 +67,73 @@ class SearchStudent extends React.Component {
             });
     };
 
+    createStudent = () => {};
+
     /* Student Search View - Admin Mode */
-    renderAdminModeView = () => {
+    newStudentForm = () =>
+        [<h1 className="search_form_title">create a new student</h1>,
+         <Grid container direction="row" justify="center" alignItems="center">
+             <Input
+                 className="new_team__input"
+                 name="newStudentName"
+                 value={this.state.newStudentName}
+                 id="filled-textarea"
+                 label={"Name"}
+                 onChange={this.handleSearchInput}
+                 multiline
+             />
+             <Input
+                 className="new_team__input"
+                 name="newStudentUniversity"
+                 value={this.state.newStudentUniversity}
+                 id="filled-textarea"
+                 label={"University"}
+                 onChange={this.handleSearchInput}
+                 multiline
+             />
+             <Button
+                 variant="outlined"
+                 color="primary"
+                 className="new_team_button"
+                 onClick={this.createStudent}
+             >
+                 Create
+             </Button>
+         </Grid>];
 
-        return (
-            <div>
-                <div className="search_student_view">
-                    <div>
-                        <h1 className="search_form_title">create a new student</h1>
-                        <Grid container direction="row" justify="center" alignItems="center">
-                            <Input
-                                className="new_team__input"
-                                name="newStudentName"
-                                value={this.state.newStudentName}
-                                id="filled-textarea"
-                                label={"Name"}
-                                onChange={this.handleSearchInput}
-                                multiline
-                            />
-                            <Input
-                                className="new_team__input"
-                                name="newStudentUniversity"
-                                value={this.state.newStudentUniversity}
-                                id="filled-textarea"
-                                label={"University"}
-                                onChange={this.handleSearchInput}
-                                multiline
-                            />
-                            <Button
-                                variant="outlined"
-                                color="primary"
-                                className="new_team_button"
-                                onClick={this.createStudent}
-                            >
-                                Create
-                            </Button>
-                            <NotificationContainer/>
-                        </Grid>
-                    </div>
 
-                    <h1 className="search_form_title">search students</h1>
-                    {/* Students Search Form */}
-                    <div className="search-form">
-                        <SearchStudentForm
-                            studentName={this.state.studentName}
-                            studentUniversity={this.state.studentUniversity}
-                            studentCourse={this.state.studentCourse}
-                            handleSearch={this.handleSearchInput}
-                            filterStudents={this.filterStudents}
-                        />
-                    </div>
-                    <div className="students-list__admin-view">
-                    {/* Student Previews Table*/}
-                    {this.state.filteredStudents.map(student => (
-                        <div key={uid(
-                            student
-                        )}>
-                            <Link className="team_page__link" to={'/student-profile/' + student.uid}>
-                            {student.name}
-                            </Link>
-                            <button
-                                className="team_page__button"
-                                onClick={this.removeStudent.bind(this, student)}>remove
-                            </button>
-                        </div>
-                    ))}
-                    </div>
-                </div>
-            </div>
-        )
-    };
 
     /* Student Search View - Student Mode */
     render() {
 
         const global = this.props.state;
+        const isAdmin = global.identity.type === "admin";
+        const appendActions =
+              isAdmin
+              && (student =>
+                  <button
+                      className="team_page__button"
+                      onClick={() => this.removeStudent(student)}>
+                      remove
+                  </button>);
 
         return (
-            global.identity.type === "admin" ?
-                this.renderAdminModeView()
-                :
             <div>
                 <div className="search_student_view">
-                    <h1 className="search_form_title">find a partner</h1>
+                    <h1 className="search_form_title">
+                        { isAdmin
+                          ? 'manage students'
+                          : 'find a partner' }
+                    </h1>
+
+                    { isAdmin &&
+                      [this.newStudentForm(),
+                       <h1 className='search_form_title'>
+                           search students
+                       </h1>
+                      ] }
+
+                    <NotificationContainer />
 
                     {/* Students Search Form */}
                     <div className="search-form">
@@ -166,7 +149,9 @@ class SearchStudent extends React.Component {
                     {/* Student Previews Table*/}
                     <div className="search_student_member_view">
                         <TeamMemberPreviewList
-                            members={this.state.filteredStudents}/>
+                            members={this.state.filteredStudents}
+                            append={appendActions}
+                        />
                     </div>
                 </div>
             </div>
